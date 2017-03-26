c503db440617fd5fb2ed2e231f661c0af385a421
billofbong <willjwright@mac.com>
2017-03-25 20:21:33 -0700

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import hs.core.PhysicsSimulator;
+
+/**
+ * @author Will Wright
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        new PhysicsSimulator().start();
+    }
+}
