adaaa64069ab93ab567edfa42dcf471c77f3b650
billofbong <willjwright@mac.com>
2017-03-25 18:57:16 -0700

    Made Vector2 changes

--- src/hs/physics/Vector2.java
+++ src/hs/physics/Vector2.java
@@ -7,4 +7,57 @@ package hs.physics;
 public class Vector2
 {
 
+    private float x;
+    private float y;
+
+    public Vector2(float x, float y)
+    {
+        this.x = x;
+        this.y = x;
+    }
+
+    public float getX()
+    {
+        return x;
+    }
+
+    public void setX(float x)
+    {
+        this.x = x;
+    }
+
+    public float getY()
+    {
+        return y;
+    }
+
+    public void setY(float y)
+    {
+        this.y = y;
+    }
+
+    public void add(Vector2 v2)
+    {
+        setX(this.getX() * v2.getX());
+        setY(this.getX() * v2.getY());
+    }
+
+    public Vector2 createAddedVector(Vector2 ... vector2s)
+    {
+        Vector2 finalVector = new Vector2(0 ,0,);
+
+        for(Vector2 v : vector2s)
+        {
+            finalVector.add(v);
+        }
+
+        return finalVector;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "{" + getX() + ", " + getY() + "}";
+    }
+
 }
